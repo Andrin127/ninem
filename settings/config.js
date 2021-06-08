ffc5a3e899f2971807de57928168667b94cf2d8a
Andrin127 <79515029+Andrin127@users.noreply.github.com>
2021-06-08 11:22:38 -0300

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'Bot Turma 83',
+        ownerName: 'Andrin',
+        youtube: 'https://www.youtube.com/channel/UC_e39TuTSh7xzFSizwv-q5g',
+        Site: 'https://turma83murialdo.wixsite.com/turma',
+}
---
+++ src/nabutomenu.js
@@ -0,0 +1,12 @@
+const nabutomenu = (prefix, pushname) => {
+    return `◪ *Comandos do Andrin*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.nabutomenu = nabutomenu
